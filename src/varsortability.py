139bd5c10de4785171b0b145c0f34efec9697747
Scriddie <alexander.reisach@gmail.com>
2022-04-08 10:40:36 +0200

    vsb resolve equal variances

--- src/varsortability.py
+++ src/varsortability.py
@@ -1,7 +1,6 @@
 import numpy as np
 
-
-def varsortability(X, W):
+def varsortability(X, W, tol=1e-9):
     """ Takes n x d data and a d x d adjaceny matrix,
     where the i,j-th entry corresponds to the edge weight for i->j,
     and returns a value indicating how well the variance order
@@ -9,20 +8,24 @@ def varsortability(X, W):
     E = W != 0
     Ek = E.copy()
     var = np.var(X, axis=0, keepdims=True)
-    tol = var.min() * 1e-9
 
     n_paths = 0
     n_correctly_ordered_paths = 0
 
-    for k in range(E.shape[0] - 1):
+    for _ in range(E.shape[0] - 1):
         n_paths += Ek.sum()
         n_correctly_ordered_paths += (Ek * var / var.T > 1 + tol).sum()
+        n_correctly_ordered_paths += 1/2*(
+            (Ek * var / var.T <= 1 + tol) *
+            (Ek * var / var.T >  1 - tol)).sum()
         Ek = Ek.dot(E)
 
     return n_correctly_ordered_paths / n_paths
-
 
 if __name__ == "__main__":
     W = np.array([[0, 1, 0], [0, 0, 2], [0, 0, 0]])
     X = np.random.randn(1000, 3).dot(np.linalg.inv(np.eye(3) - W))
     print("Varsortability:", varsortability(X, W))
+    
+    X_std = (X - np.mean(X, axis=0))/np.std(X, axis=0)
+    print("Varsortability standardized:", varsortability(X_std, W))
